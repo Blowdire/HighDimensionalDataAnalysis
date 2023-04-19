ab22717773bc8e9d1e903a83015e935efa349ed3
Blowdire <agazzi.ruben99@gmail.com>
2023-04-19 19:05:18 +0200

    capitolo 5.7 che va

---
+++ test7.R
@@ -0,0 +1,63 @@
+library(caret)
+library(C50)
+library(pROC)
+library(randomUniformForest)
+
+###################################################################
+
+seed <- 45
+
+read.csv("car.data")
+
+
+###################################################################
+
+
+ctrl <- trainControl(method = "cv", 
+                     classProbs = TRUE,
+                     summaryFunction = mnLogLoss)
+
+###################################################################
+
+set.seed(seed)
+in_train <- createDataPartition(carEvaluation$class, p = 3/4, list = FALSE)
+training <- carEvaluation[ in_train, ]
+testing  <- carEvaluation[-in_train, ]
+
+mod <- train(class ~ ., data = training, 
+             method = "C5.0",
+             tuneGrid = data.frame(trials = c(1:20, 10*(3:10)),
+                                   model = "rules",
+                                   winnow = FALSE),
+             metric = "logLoss",
+             trControl = ctrl)
+
+###################################################################
+
+test_pred <- predict(mod, testing, type = "prob")
+test_pred$pred <- predict(mod, testing)
+test_pred$obs <- testing$class
+
+test_res <- multiClassSummary(test_pred, lev = levels(factor(test_pred$obs)))
+test_res <- data.frame(t(test_res))
+test_res$Data <- "Car Evaluation"
+test_res$Model <- "Boosted C5.0 Rules"
+test_res$Ordered <- "None"
+test_res$Seed <- seed
+test_res$Encoding <- "Dummy Variables"
+test_res$Time <- mod$times$everything[3]
+
+###################################################################
+
+rs_res <- mod$resample
+rs_res$Data <- "Car Evaluation"
+rs_res$Model <- "Boosted C5.0 Rules"
+rs_res$Ordered <- "None"
+rs_res$Seed <- seed
+rs_res$Encoding <- "Dummy Variables"
+
+###################################################################
+
+save(test_res, rs_res,
+     file = file.path("./", "Salvataggi",
+                      paste0("C5boostrules_dummy_", seed, ".RData")))
